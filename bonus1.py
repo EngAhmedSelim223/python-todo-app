17bc907cc8ea7f73d176f47998f82970d83bb0e9
Ahmed Selim <ahmedselimelsayed@gmail.com>
2025-06-26 12:05:34 +0300

    commit 03

--- bonus1.py
+++ bonus1.py
@@ -1,12 +1,3 @@
-name = input("Enter your name: ").lower().strip()
-match name:
-    case "ahmed":
-        print("You are 30 years old")
-    case "mahmoud":
-        print("You are 26 years old")
-    case "thanaa":
-        print("you are 23 years old")
-    case "zaki" | "alyaa":
-        print("you are 20 years old")
-    case _:
-        print("i don't know your age")
+filenames = ["1.Raw data.txt", "2.Raw data.txt", "3.Raw data.txt"]
+for filename in filenames:
+    print(filename)
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 todos = []
 while True:
-    user_action = input("Type add or show or exit: ").lower().strip()
+    user_action = input("Type add, show, edit or exit: ").lower().strip()
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
-            todos.append(todo)
+            todos.append(f"{len(todos) + 1}- {todo}")
         case "show":
             for item in todos:
                 print(item)
         case "exit":
             print("Bye! :)")
             break
+        case "edit":
+            print(todos)
+            todo_number = int(input("Enter the number of todo to edit: "))
+            new_todo = input("Enter the new todo: ")
+            todos[todo_number - 1] = f"{todo_number}- {new_todo}"
         case _:
             print("Unknown action...")
 
