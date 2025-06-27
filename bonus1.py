09aa812cf14fd520526ec25f579dc38f5a7607ab
Ahmed Selim <ahmedselimelsayed@gmail.com>
2025-06-27 19:57:53 +0300

    commit 05

--- bonus1.py
+++ bonus1.py
@@ -1,10 +1,4 @@
-info = [
-    ("Ahmed", 30, "Data Engineer"),
-    ("Sara", 27, "Software Developer"),
-    ("John", 35, "Project Manager"),
-    ("Emily", 29, "Data Analyst"),
-    ("Michael", 32,)
-]
-
-for name, age, job in info:
-    print(f"Your name is: {name}, and age is: {age}, you work as: {job}")
+member = input("Enter a new member: ")
+file = open("members.txt", "a")
+file.write(member + "\n")
+file.close()
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-todos = []
+read_file = open("todos.txt", "r")
+todos = read_file.readlines()
+read_file.close()
 while True:
     user_action = input("Type add, show, edit, complete or exit: ").lower().strip()
     match user_action:
         case "add":
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
             todos.append(todo)
+            write_file = open("todos.txt", "w")
+            write_file.writelines(todos)
+            write_file.close()
         case "show":
             for index, item in enumerate(todos):
-                print(f"{index + 1}- {item}")
+                print(f"{index + 1}- {item.strip()}")
         case "exit":
             print("Bye! :)")
             break
@@ -16,10 +21,16 @@ while True:
             todo_number = int(input("Enter the number of todo to edit: "))
             new_todo = input("Enter the new todo: ")
             todos[todo_number - 1] = new_todo
+            write_file = open("todos.txt", "w")
+            write_file.writelines(todos)
+            write_file.close()
         
         case "complete":
             todo_number = int(input("Enter the number of todo to complete: "))
             todos.pop(todo_number - 1)
+            write_file = open("todos.txt", "w")
+            write_file.writelines(todos)
+            write_file.close()
         case _:
             print("Unknown action...")
 
