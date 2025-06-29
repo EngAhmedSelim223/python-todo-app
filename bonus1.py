4f558d172c025269f8864188dc203875ca2e5a02
Ahmed Selim <ahmedselimelsayed@gmail.com>
2025-06-29 14:45:03 +0300

    commit 09

--- bonus1.py
+++ bonus1.py
@@ -1,23 +1,30 @@
-password = input("Enter Password to check: ")
-check_password = {}
-if(len(password) <= 8):
-    check_password['length'] = False
-else:
-    check_password['length'] = True
+# password = input("Enter Password to check: ")
+# check_password = {}
+# if(len(password) <= 8):
+#     check_password['length'] = False
+# else:
+#     check_password['length'] = True
     
-is_digit = False
-for c in password:
-    if c.isdigit():
-        is_digit = True
-check_password['contains_digit'] = is_digit
+# is_digit = False
+# for c in password:
+#     if c.isdigit():
+#         is_digit = True
+# check_password['contains_digit'] = is_digit
 
-is_upper = False
-for c in password:
-    if c.isupper():
-        is_upper = True
-check_password['contains_upper'] = is_upper
+# is_upper = False
+# for c in password:
+#     if c.isupper():
+#         is_upper = True
+# check_password['contains_upper'] = is_upper
 
-if(all(check_password.values())):
-    print("Strong Password")
+# if(all(check_password.values())):
+#     print("Strong Password")
+# else:
+#     print("Weak Password")
+  
+width = float(input("Enter Width: "))
+height = float(input("Enter height: "))
+if width == height:
+    exit("This is a square not rectangle")
 else:
-    print("Weak Password")
+    print(f"the area is : {width * height}")
--- main.py
+++ main.py
@@ -5,33 +5,40 @@ with open("todos.txt", "r") as read_file:
 while True:
     user_action = input("Type add, show, edit, complete or exit: ").lower().strip()
     
-    if "add" in user_action or "new" in user_action:
+    if user_action.startswith("add") :
         todo = user_action[4:] + "\n"
         todos.append(todo)
         with open("todos.txt", "w") as write_file:
             write_file.writelines(todos)
             
-    elif "show" in user_action or "get" in user_action:
+    elif user_action.startswith("show"):
         for index, item in enumerate(todos):
             print(f"{index + 1}- {item.strip()}")
             
-    elif "exit" in user_action:
+    elif user_action.startswith("exit"):
         print("Bye! :)")
         break
     
-    elif "edit" in user_action:
-        todo_index = int(user_action.strip()[-1]) - 1
-        new_todo = input("Enter the new todo: ") + "\n"
-        todos[todo_index] = new_todo
-        with open("todos.txt", "w") as write_file:
-            write_file.writelines(todos)
-    
-    elif "complete" in user_action or "done" in user_action:
-        todo_index = int(user_action.strip()[-1]) - 1
-        removed_todo = todos.pop(todo_index)
-        print(f"this todo: [{removed_todo.strip()}] is removed from the list")
-        with open("todos.txt", "w") as write_file:
-            write_file.writelines(todos)
+    elif user_action.startswith("edit"):
+        
+        try:
+            todo_index = int(user_action.strip()[-1]) - 1
+            new_todo = input("Enter the new todo: ") + "\n"
+            todos[todo_index] = new_todo
+            with open("todos.txt", "w") as write_file:
+                write_file.writelines(todos)
+        except ValueError:
+            print("Invalid command Pleas enter edit + todo number")
+            
+    elif user_action.startswith("complete"):
+        try:
+            
+            todo_index = int(user_action.strip()[-1]) - 1
+            removed_todo = todos.pop(todo_index)
+            print(f"this todo: [{removed_todo.strip()}] is removed from the list")
+            with open("todos.txt", "w") as write_file:
+                write_file.writelines(todos)
+        except ValueError:
+            print("invalid command please enter complete + todo number")
     else:
         print("Command is not valid..")
-
