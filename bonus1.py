416c498165afe7990df238edb6925366d38c27c6
Ahmed Selim <ahmedselimelsayed@gmail.com>
2025-06-29 12:06:51 +0300

    commit 08

--- bonus1.py
+++ bonus1.py
@@ -1,3 +1,23 @@
-from datetime import date
+password = input("Enter Password to check: ")
+check_password = {}
+if(len(password) <= 8):
+    check_password['length'] = False
+else:
+    check_password['length'] = True
+    
+is_digit = False
+for c in password:
+    if c.isdigit():
+        is_digit = True
+check_password['contains_digit'] = is_digit
 
-print("Today's date:", date.today())
+is_upper = False
+for c in password:
+    if c.isupper():
+        is_upper = True
+check_password['contains_upper'] = is_upper
+
+if(all(check_password.values())):
+    print("Strong Password")
+else:
+    print("Weak Password")
--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 
 with open("todos.txt", "r") as read_file:
     todos = read_file.readlines()
+    
 while True:
     user_action = input("Type add, show, edit, complete or exit: ").lower().strip()
-    if "add" in user_action:
+    
+    if "add" in user_action or "new" in user_action:
         todo = user_action[4:] + "\n"
         todos.append(todo)
         with open("todos.txt", "w") as write_file:
             write_file.writelines(todos)
-    if "show" in user_action:
+            
+    elif "show" in user_action or "get" in user_action:
         for index, item in enumerate(todos):
             print(f"{index + 1}- {item.strip()}")
-    if "exit" in user_action:
+            
+    elif "exit" in user_action:
         print("Bye! :)")
         break
-    if "edit" in user_action:
-        print(todos)
-        todo_number = int(input("Enter the number of todo to edit: "))
+    
+    elif "edit" in user_action:
+        todo_index = int(user_action.strip()[-1]) - 1
         new_todo = input("Enter the new todo: ") + "\n"
-        todos[todo_number - 1] = new_todo
+        todos[todo_index] = new_todo
         with open("todos.txt", "w") as write_file:
             write_file.writelines(todos)
     
-    if "complete" in user_action:
-        todo_number = int(input("Enter the number of todo to complete: "))
-        removed_todo = todos.pop(todo_number - 1)
-        print(f"this todo: [{removed_todo.strip()} is removed from the list")
+    elif "complete" in user_action or "done" in user_action:
+        todo_index = int(user_action.strip()[-1]) - 1
+        removed_todo = todos.pop(todo_index)
+        print(f"this todo: [{removed_todo.strip()}] is removed from the list")
         with open("todos.txt", "w") as write_file:
             write_file.writelines(todos)
+    else:
+        print("Command is not valid..")
 
