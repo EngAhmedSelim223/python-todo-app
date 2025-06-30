f0fd3d4d6dcb39ee43a64c05950df54e3b9a31ac
Ahmed Selim <ahmedselimelsayed@gmail.com>
2025-06-30 11:33:12 +0300

    commit 10

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+filename = "todos.txt"
+def get_todos(filename):
+    with open(filename, "r") as read_file:
+        todos = read_file.readlines()
+    return todos
 
-with open("todos.txt", "r") as read_file:
-    todos = read_file.readlines()
-    
+def update_todos(filename, todos):
+    with open(filename, "w") as write_file:
+        write_file.writelines(todos)
+
+todos = get_todos(filename)
+
 while True:
     user_action = input("Type add, show, edit, complete or exit: ").lower().strip()
     
     if user_action.startswith("add") :
         todo = user_action[4:] + "\n"
         todos.append(todo)
-        with open("todos.txt", "w") as write_file:
-            write_file.writelines(todos)
+        update_todos(filename, todos)
             
     elif user_action.startswith("show"):
         for index, item in enumerate(todos):
@@ -25,8 +32,7 @@ while True:
             todo_index = int(user_action.strip()[-1]) - 1
             new_todo = input("Enter the new todo: ") + "\n"
             todos[todo_index] = new_todo
-            with open("todos.txt", "w") as write_file:
-                write_file.writelines(todos)
+            update_todos(filename, todos)
         except ValueError:
             print("Invalid command Pleas enter edit + todo number")
             
@@ -36,8 +42,7 @@ while True:
             todo_index = int(user_action.strip()[-1]) - 1
             removed_todo = todos.pop(todo_index)
             print(f"this todo: [{removed_todo.strip()}] is removed from the list")
-            with open("todos.txt", "w") as write_file:
-                write_file.writelines(todos)
+            update_todos(filename, todos)
         except ValueError:
             print("invalid command please enter complete + todo number")
     else:
