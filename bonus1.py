3447fbf0b38d76db6c27ea7636a0dc2a35a31eb7
Ahmed Selim <ahmedselimelsayed@gmail.com>
2025-07-01 13:18:22 +0300

    commit 12

--- bonus1.py
+++ bonus1.py
@@ -1,14 +1,17 @@
 feet_inches = input("enter feet and inches: ")
-
-def convert(feet_inches):
+def parse(feet_inches):
     result = [x for x in feet_inches.strip().split(" ")]
     feet = float(result[0])
     inches = float(result[-1])
+    return (feet, inches)
+
+def convert(feet, inches):
+    
     meters = feet * 0.3048 + inches * 0.0254
     return meters
-
-result = convert(feet_inches= feet_inches)
+feet, inches = parse(feet_inches)
+result = convert(feet , inches)
 if result < 1:
-    print("Kid can't user slider")
+    print(f"tall: {result} => Kid can't user slider")
 else:
-    print("Kid can use slider")
+    print(f"tall: {result} => Kid can use slider")
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-filename = "todos.txt"
 # NOTE: Get Todos
-def get_todos(filename):
+def get_todos(filename = "todos.txt"):
+    """ 
+        function used to read the text file
+        and return a to-do list
+    """
     with open(filename, "r") as read_file:
         todos = read_file.readlines()
     return todos
+todos = get_todos()
 # NOTE: Update Todos
-def update_todos(filename, todos):
+def update_todos(filename = "todos.txt", todos = todos):
+    """ 
+        function to write to-do item list
+        to the text file
+    """
     with open(filename, "w") as write_file:
         write_file.writelines(todos)
 
-todos = get_todos(filename)
+todos = get_todos()
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ").lower().strip()
@@ -17,7 +25,7 @@ while True:
     if user_action.startswith("add") :
         todo = user_action[4:] + "\n"
         todos.append(todo)
-        update_todos(filename, todos)
+        update_todos()
             
     elif user_action.startswith("show"):
         for index, item in enumerate(todos):
@@ -33,7 +41,7 @@ while True:
             todo_index = int(user_action.strip()[-1]) - 1
             new_todo = input("Enter the new todo: ") + "\n"
             todos[todo_index] = new_todo
-            update_todos(filename, todos)
+            update_todos()
         except ValueError:
             print("Invalid command Pleas enter edit + todo number")
             
@@ -43,7 +51,7 @@ while True:
             todo_index = int(user_action.strip()[-1]) - 1
             removed_todo = todos.pop(todo_index)
             print(f"this todo: [{removed_todo.strip()}] is removed from the list")
-            update_todos(filename, todos)
+            update_todos()
         except ValueError:
             print("invalid command please enter complete + todo number")
     else:
