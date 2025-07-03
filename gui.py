3508205e749dfdd42f593470d774c3e6b3842fb4
Ahmed Selim <ahmedselimelsayed@gmail.com>
2025-07-03 13:47:21 +0300

    GUI cont.

--- gui.py
+++ gui.py
@@ -1,8 +1,39 @@
 from modules import functions 
 import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip = "Enter todo")
+input_box = sg.InputText(tooltip = "Enter todo", key="todo")
 add_button = sg.Button("Add")
-window = sg.Window("My To-Do App", layout=[[label], [input_box, add_button]])
-window.read()
+list_box = sg.Listbox(values = functions.get_todos(), key = "todos",
+                      enable_events = True, size = [45, 10])
+
+edit_button = sg.Button("Edit")
+layout = [[label], [input_box, add_button], [list_box, edit_button]]
+
+window = sg.Window("My To-Do App",
+                   layout= layout,
+                   font=("Helvetica", 12))
+
+while True:
+    event, values = window.read()
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo']
+            todos.append(new_todo + "\n")
+            functions.update_todos(todos = todos)
+            print(todos)
+        
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.update_todos(todos = todos)
+            window['todos'].Update(values = todos)
+        case "todos":
+            window['todo'].Update(value = values['todos'][0])
+        case sg.WIN_CLOSED:
+            break
 window.close()
