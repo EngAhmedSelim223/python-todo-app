64b21268c07469baf49a4311afb819e73923e9dc
Ahmed Selim <ahmedselimelsayed@gmail.com>
2025-07-03 14:42:37 +0300

    GUI cont.2

--- gui.py
+++ gui.py
@@ -7,7 +7,14 @@ list_box = sg.Listbox(values = functions.get_todos(), key = "todos",
                       enable_events = True, size = [45, 10])
 
 edit_button = sg.Button("Edit")
-layout = [[label], [input_box, add_button], [list_box, edit_button]]
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+layout = [
+    [label],
+    [input_box, add_button],
+    [list_box, edit_button, complete_button],
+    [exit_button]
+    ]
 
 window = sg.Window("My To-Do App",
                    layout= layout,
@@ -15,25 +22,43 @@ window = sg.Window("My To-Do App",
 
 while True:
     event, values = window.read()
-    print(values)
+
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo']
             todos.append(new_todo + "\n")
             functions.update_todos(todos = todos)
-            print(todos)
         
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] + "\n"
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.update_todos(todos = todos)
-            window['todos'].Update(values = todos)
+           try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo'] + "\n"
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.update_todos(todos = todos)
+                window['todos'].Update(values = todos)
+           except IndexError:
+               sg.popup("Please Select an item First")
+               
         case "todos":
             window['todo'].Update(value = values['todos'][0])
+        
+        case "Complete":
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                index = todos.index(todo_to_complete)
+                todos.pop(index)
+                functions.update_todos(todos = todos)
+                window["todos"].Update(values = todos)
+            except IndexError:
+               sg.popup("Please Select an item First")
+        
+        case "Exit":
+            break
+        
         case sg.WIN_CLOSED:
             break
 window.close()
